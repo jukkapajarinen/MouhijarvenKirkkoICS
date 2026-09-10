e37001059d76c55e2c52445d0c33bb56acddf180
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2026-09-11 02:04:50 +0300

    end one sec later than start

--- scripts/generate-ics.js
+++ scripts/generate-ics.js
@@ -99,7 +99,7 @@ function toEvent(entry) {
     `SUMMARY:${escapeIcsText(title)}`,
     `DTSTAMP:${toUtcStamp(new Date())}`,
     `DTSTART:${toUtcStamp(start)}`,
-    `DTEND:${toUtcStamp(start)}`,
+    `DTEND:${toUtcStamp(new Date(start.getTime() + 1000))}`,
     `DESCRIPTION:${escapeIcsText(description)}`,
   ];
   if (url) {
