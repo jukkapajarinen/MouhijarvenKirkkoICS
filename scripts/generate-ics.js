478d521861172730460c6c9e44f3fb103e068698
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2026-09-11 02:27:49 +0300

    remove dtend

--- scripts/generate-ics.js
+++ scripts/generate-ics.js
@@ -99,7 +99,6 @@ function toEvent(entry) {
     `SUMMARY:${escapeIcsText(title)}`,
     `DTSTAMP:${toUtcStamp(new Date())}`,
     `DTSTART:${toUtcStamp(start)}`,
-    `DTEND:${toUtcStamp(new Date(start.getTime() + 60000))}`,
     `DESCRIPTION:${escapeIcsText(description)}`,
   ];
   if (url) {
